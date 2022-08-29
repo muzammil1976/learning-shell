cb4b3b1e87531664be19f63ab09ab0e137a54979
muzammil1976 <muzammil.shah@gmail.com>
2022-08-29 18:40:02 -0500

    print demo

---
+++ 04-read.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+read -p 'Enter your name: ' name
+echo -e "Hey $name, welcome to DevOps Training..."
+
+read -p 'enter your phone number: ' phone
+echo "Thanks for entering details, Will text you details soon to$phone"
